54318899bdbf3d3f2c870cb5639dbda737e621f5
Paulo Ricardo <paulorihcardo18@gmail.com>
2025-06-27 02:16:33 -0300

    add das info para swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,23 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     version="1.0",
+ *     title="API Documentation Book",
+ *     description="Description removed for better illustration of structure",
+ *     @OA\Contact(
+ *         email="paulorihcardo18@gmail.com",
+ *         name="Ricardo",
+ *         url="https://github.com/devricardoo",
+ * ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html",
+ *     )
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
