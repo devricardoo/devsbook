ac89a47658c27b5ab520995ea77092d52eec38da
Paulo Ricardo <paulorihcardo18@gmail.com>
2025-06-27 00:50:58 -0300

    mudança em algumas api

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ping', function () {
-    return ['pong' => true];
-});
 Route::get('/401', 'App\Http\Controllers\AuthController@unauthorized')->name('login');
 
 Route::post('/auth/login', 'App\Http\Controllers\AuthController@login');
 Route::post('/auth/logout', 'App\Http\Controllers\AuthController@logout');
 Route::post('/auth/refresh', 'App\Http\Controllers\AuthController@refresh');
 
-Route::post('/user', 'App\Http\Controllers\AuthController@create');
+Route::post('/devbook/user', 'App\Http\Controllers\AuthController@create');
 
 Route::put('/user', 'App\Http\Controllers\UserController@update');
 Route::post('/user/avatar', 'App\Http\Controllers\UserController@updateAvatar');
