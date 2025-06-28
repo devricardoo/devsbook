b62fe017c6ea86d1e4fd32d547b4be00ea45b889
Paulo Ricardo <paulorihcardo18@gmail.com>
2025-06-27 22:44:24 -0300

    documentation swagger para filtragem de usuários

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,6 +6,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
+/**
+ * @OA\Tag(
+ *     name="Search",
+ *     description="Authentication endpoints"
+ */
+
+/**
+ */
+
 class SearchController extends Controller
 {
     private $loggedUser;
@@ -15,6 +24,48 @@ class SearchController extends Controller
         $this->middleware('auth:api');
         $this->loggedUser = auth()->user();
     }
+
+    /**
+     * @OA\Get(
+     *     path="/api/search",
+     *     tags={"Search"},
+     *     summary="Filtrar usuários",
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="txt",
+     *         in="query",
+     *         required=true,
+     *         description="Texto de busca para filtrar os usuários",
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Busca realizada com sucesso",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="users",
+     *                 type="array",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(property="id", type="integer"),
+     *                     @OA\Property(property="name", type="string"),
+     *                     @OA\Property(property="avatar", type="string", format="url")
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Texto de busca não informado",
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Erro na busca"
+     *     )
+     * )
+     */
+
 
     public function search(Request $request)
     {
